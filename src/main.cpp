435bc4a7ab5309b3c397b67eabfa30ab05784ca2
ArthurM <arthurmagnien@gmail.com>
2013-05-13 08:54:02 +0200

    Test arthur

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, const char * argv[])
     cout << "Welcome on Sokaris!\n";
 
 #if defined(_WIN32) || defined(_WIN64)
-	cout << "je m'appelle Greg" << endl;
+	cout << "je m'appelle Arthur" << endl;
 	system("pause");
 #endif // _WIN32 || _WIN64
     

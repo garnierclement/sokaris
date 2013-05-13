70fa24af1d334e69225e372568520444a1ccb9c5
CMW <900930@gmail.com>
2013-05-13 09:58:35 +0200

    First implementation Map

--- src/Map.cpp
+++ src/Map.cpp
@@ -7,3 +7,65 @@
 //
 
 #include "Map.h"
+
+using namespace std;
+namespace sokaris
+{
+    Map::Map()
+    {
+    }
+    
+    Map::~Map()
+    {
+    }
+
+	int Map::setCoordinates(vector<Point3d*> listCoordinates, string filename)
+	{
+		FileStorage fs(filename, FileStorage::WRITE);
+		fs << "Map";										// text - mapping
+        fs << "[" << "Test1" << "Test2" << "]";     
+		fs.release();
+		return 0;
+	}
+
+	int Map::getCoordinates(string filename)
+	{
+		FileStorage fs;
+        fs.open(filename, FileStorage::READ);
+		FileNode n = fs["Map"];
+		if (n.type() != FileNode::SEQ)
+            return -1;										// Map n'est pas une séquence
+
+		FileNodeIterator it = n.begin(), it_end = n.end();  // Itérateur pour lire la séquence
+        for (; it != it_end; ++it)
+            cout << (string)*it << endl;
+
+		fs.release();										// énoncer la fermeture
+		return 0;
+	}
+
+	int Map::setCameras(vector<Camera*> listCoordinates, string filename)
+	{
+		FileStorage fs(filename, FileStorage::WRITE);
+		fs << "Camera";
+        fs << "[" << "Test1" << "Test2" << "]";     
+		fs.release();
+		return 0;
+	}
+
+	int Map::getCameras(string filename)
+	{
+		FileStorage fs;
+        fs.open(filename, FileStorage::READ);
+		FileNode n = fs["Camera"];
+		if (n.type() != FileNode::SEQ)
+            return -1;										// Map n'est pas une séquence
+
+		FileNodeIterator it = n.begin(), it_end = n.end();  // Itérateur pour lire la séquence
+        for (; it != it_end; ++it)
+            cout << (string)*it << endl;
+
+		fs.release();										// énoncer la fermeture
+		return 0;
+	}
+}
--- src/Map.h
+++ src/Map.h
@@ -10,15 +10,34 @@
 #define __sokaris__Map__
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+
+#include "Camera.h"
 
 namespace sokaris
 {
-    class Map
-    {
-        public :
-        
-        
-    };
+	class Map
+	{
+	private :
+		vector<Camera*> listOfCameras;			// Liste des caméras dans le plan
+		vector<Point3d*> listOfCoordinates;		// Liste des points cruciaux pour le modèle 3D
+
+	public :
+		int getCoordinates(string filename);	// Depuis un fichier xml
+		int setCoordinates(vector<Point3d*> listCoordinates, string filename);
+												// Stocker dans un fichier xml
+		int getCameras(string filename);		// Depuis un fichier xml
+		int setCameras(vector<Camera*> listCoordinates, string filename);
+												// Stocker dans un fichier xml
+		void drawMap();
+		Map(void);
+		~Map(void);
+
+	};
+
 }
 
 #endif /* defined(__sokaris__Map__) */

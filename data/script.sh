f6fcfc3738a0cb77e06bd44ac728687c062c8051
Clément Garnier <garnierclement@yahoo.fr>
2013-05-22 14:02:27 +0200

    modification script

--- data/script.sh
+++ data/script.sh
@@ -10,6 +10,7 @@ read -p "Did you put samples in negative folder ? (y/n)" -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	rm bg.txt
     find negative -name '*.jpg' > bg.txt
     echo "File descriptor for negative generated"
 fi
@@ -18,7 +19,12 @@ read -p "Did you put samples in positive folder ? (y/n)" -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	rm info.dat
     find positive -name '*.jpg' -exec /usr/local/Cellar/imagemagick/6.8.0-10/bin/identify -format '%i 1 0 0 %w %h' \{\} \; > info.dat
     echo "File descriptor for positive generated"
 fi
+
+rm out.vec
+
+opencv_createsamples -vec out.vec -info info.dat -bg bg.txt
 

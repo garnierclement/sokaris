3b9d58a5c17f2f375dc5b335ae87d54fb5419092
Clément Garnier <garnierclement@yahoo.fr>
2013-05-22 19:10:22 +0200

    correction chemin vers identify

--- data/script.sh
+++ data/script.sh
@@ -4,7 +4,7 @@ echo "Training"
 
 mkdir negative			# folder with negative images
 mkdir positive			# folder with positive images
-mkdir samples			# folder with generated samples
+#mkdir samples			# folder with generated samples
 
 read -p "Did you put samples in negative folder ? (y/n)" -n 1 -r
 echo ""
@@ -20,7 +20,7 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	rm info.dat
-    find positive -name '*.jpg' -exec /usr/local/Cellar/imagemagick/6.8.0-10/bin/identify -format '%i 1 0 0 %w %h' \{\} \; > info.dat
+    find positive -name '*.jpg' -exec identify -format '%i 1 0 0 %w %h' \{\} \; > info.dat
     echo "File descriptor for positive generated"
 fi
 

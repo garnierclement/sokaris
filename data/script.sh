4186431dad69faeee4a27d0135992abbec8fd5ac
Clément Garnier <garnierclement@yahoo.fr>
2013-05-22 11:46:22 +0200

    mise à jour script pour positive et info.dat à jour

---
+++ data/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Training"
+
+mkdir negative			# folder with negative images
+mkdir positive			# folder with positive images
+mkdir samples			# folder with generated samples
+
+read -p "Did you put samples in negative folder ? (y/n)" -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    find negative -name '*.jpg' > bg.txt
+    echo "File descriptor for negative generated"
+fi
+
+read -p "Did you put samples in positive folder ? (y/n)" -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    find positive -name '*.jpg' -exec /usr/local/Cellar/imagemagick/6.8.0-10/bin/identify -format '%i 1 0 0 %w %h' \{\} \; > info.dat
+    echo "File descriptor for positive generated"
+fi
+

5d4080bcc793e12bf218e8de6150ce82b2cd6324
Clément Garnier <garnierclement@yahoo.fr>
2013-05-27 11:31:02 +0200

    Correction du test d'Arthur pour compilation sous Mac

--- src/Test.h
+++ src/Test.h
@@ -19,7 +19,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/ml/ml.hpp>
-#include "opencv2/contrib/contrib.hpp"
+#include <opencv2/contrib/contrib.hpp>
 
 using namespace std;
 using namespace cv;

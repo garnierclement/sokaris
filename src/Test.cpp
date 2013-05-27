7c56cad52040f312204099dedde800a6ef037b6b
Clément Garnier <garnierclement@yahoo.fr>
2013-05-27 14:47:48 +0200

    coorection identation

--- src/Test.cpp
+++ src/Test.cpp
@@ -55,7 +55,7 @@ namespace sokaris
         if( !face_cascade.load( path+face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
         if( !eyes_cascade.load( path+eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
         if( !mouth_cascade.load( path+mouth_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-
+        
         //-- 2. Read the video stream
         capture = cvCaptureFromCAM( -1 );
         if( capture )
@@ -86,8 +86,8 @@ namespace sokaris
 		std::vector<cv::Mat> rgbChannels(3);
 		cv::split(frame, rgbChannels);
 		cv::Mat frame_gray = rgbChannels[2];
-
-       // cvtColor( frame, frame_gray, CV_BGR2GRAY );
+        
+        // cvtColor( frame, frame_gray, CV_BGR2GRAY );
         //equalizeHist( frame_gray, frame_gray );
         
         //-- Detect faces
@@ -97,7 +97,7 @@ namespace sokaris
         //face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 		//@TODO Understand what are the arguments used in detectMultiScale
         face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(150, 150) );
-
+        
         for( int i = 0; i < faces.size(); i++ )
         {
             Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
@@ -105,7 +105,7 @@ namespace sokaris
             Mat faceROI = frame_gray( faces[i] );
             std::vector<Rect> eyes;
             std::vector<Rect> mouth;
-
+            
             //-- In each face, detect eyes
             eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
             
@@ -113,46 +113,46 @@ namespace sokaris
             {
                 Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
 				rectangle(frame,    cvPoint(faces[i].x + eyes[j].x, faces[i].y + eyes[j].y),
-									cvPoint(faces[i].x + eyes[j].x + eyes[j].width ,faces[i].y + eyes[j].y + eyes[j].height),
-                                    CV_RGB(0, 0, 255),
-                                    1, 8, 0
-                                   );
+                          cvPoint(faces[i].x + eyes[j].x + eyes[j].width ,faces[i].y + eyes[j].y + eyes[j].height),
+                          CV_RGB(0, 0, 255),
+                          1, 8, 0
+                          );
             }
-						
+            
 			//-- In each face, detect mouth
 			performFeatureDetection( mouth, faces, faceROI, mouth_cascade, frame, i);
         }
         //-- Show what you got
         imshow( window_name, frame );
     }
-
+    
 	void Test::putSomeText(String text, Mat frame, Point point) {
 		stringstream textImage("");
 		textImage << text;
 		putText(frame, textImage.str(),Point(100,400), 1, 1.0, Scalar(50,50,50));
 		cout << point << endl;
 	}
-
-	void Test::performFeatureDetection( std::vector<Rect> feature, 
-										std::vector<Rect> faces, 
-										cv::Mat faceROI, 
-										cv::CascadeClassifier feature_cascade,
-										cv::Mat frame, int i){
-		 feature_cascade.detectMultiScale( faceROI, feature, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-            
-            for( int j = 0; j < 1 /*mouth.size()*/; j++ )
-            {
-                Point center( faces[i].x + feature[j].x + feature[j].width*0.5, faces[i].y + feature[j].y + feature[j].height*0.5 );
-				Point corner = cvPoint(faces[i].x + feature[j].x + feature[j].width ,faces[i].y + feature[j].y + feature[j].height);
-				rectangle(frame,    cvPoint(faces[i].x + feature[j].x, faces[i].y + feature[j].y),
-									cvPoint(faces[i].x + feature[j].x + feature[j].width ,faces[i].y + feature[j].y + feature[j].height),
-                                    CV_RGB(225, 228, 0), //225, 228, 0
-                                    1, 8, 0
-                                   );
-				putSomeText("Mouth detected",frame, corner);
-            }
-		}
-
+    
+	void Test::performFeatureDetection( std::vector<Rect> feature,
+                                       std::vector<Rect> faces,
+                                       cv::Mat faceROI,
+                                       cv::CascadeClassifier feature_cascade,
+                                       cv::Mat frame, int i){
+        feature_cascade.detectMultiScale( faceROI, feature, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+        
+        for( int j = 0; j < 1 /*mouth.size()*/; j++ )
+        {
+            Point center( faces[i].x + feature[j].x + feature[j].width*0.5, faces[i].y + feature[j].y + feature[j].height*0.5 );
+            Point corner = cvPoint(faces[i].x + feature[j].x + feature[j].width ,faces[i].y + feature[j].y + feature[j].height);
+            rectangle(frame,    cvPoint(faces[i].x + feature[j].x, faces[i].y + feature[j].y),
+                      cvPoint(faces[i].x + feature[j].x + feature[j].width ,faces[i].y + feature[j].y + feature[j].height),
+                      CV_RGB(225, 228, 0), //225, 228, 0
+                      1, 8, 0
+                      );
+            putSomeText("Mouth detected",frame, corner);
+        }
+    }
+    
 	void Test::trainedClassifier(){
 		
 		/* STEP 2. Opening the file */
@@ -162,20 +162,20 @@ namespace sokaris
 		cvml.read_csv("samples.csv");
 		//3. Indicate which column is the response
 		cvml.set_response_idx(0);
-
+        
 		/* STEP 3. Splitting the samples */
 		//1. Select 40 for the training
 		CvTrainTestSplit cvtts(40, true);
 		//2. Assign the division to the data
 		cvml.set_train_test_split(&cvtts);
-
+        
 		printf("Training ... ");
 		/* STEP 4. The training */
 		//1. Declare the classifier
 		CvBoost boost;
 		//2. Train it with 100 features
 		boost.train(&cvml, CvBoostParams(CvBoost::REAL, 100, 0, 1, false, 0), false);
-
+        
 		/* STEP 5. Calculating the testing and training error */
 		// 1. Declare a couple of vectors to save the predictions of each sample
 		std::vector<float> train_responses, test_responses;
@@ -185,14 +185,14 @@ namespace sokaris
 		float fl2 = boost.calc_error(&cvml,CV_TEST_ERROR,&test_responses);
 		printf("Error train %f \n", fl1);
 		printf("Error test %f \n", fl2);
-
+        
 		/* STEP 6. Save your classifier */
 		// Save the trained classifier
 		boost.save("./trained_boost.xml", "boost");
-
+        
 		//return EXIT_SUCCESS;
-		}
-
+    }
+    
 	void Test::read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator) {
 		std::ifstream file(filename.c_str(), ifstream::in);
 		if (!file) {
@@ -209,80 +209,80 @@ namespace sokaris
 				labels.push_back(atoi(classlabel.c_str()));
 			}
 		}
-}
-
+    }
+    
 	void Test::findEyes(cv::Mat frame_gray, cv::Rect face) {
 		
 		cv::Mat faceROI = frame_gray(face);
-
+        
 		if (kSmoothFaceImage) {
-		double sigma = kSmoothFaceFactor * face.width;
-		GaussianBlur( faceROI, faceROI, cv::Size( 0, 0 ), sigma);
+            double sigma = kSmoothFaceFactor * face.width;
+            GaussianBlur( faceROI, faceROI, cv::Size( 0, 0 ), sigma);
 		}
 		//-- Find eye regions and draw them
 		int eye_region_width = face.width * (kEyePercentWidth/100.0);
 		int eye_region_height = face.width * (kEyePercentHeight/100.0);
 		int eye_region_top = face.height * (kEyePercentTop/100.0);
 		cv::Rect leftEyeRegion(face.width*(kEyePercentSide/100.0),
-                         eye_region_top,eye_region_width,eye_region_height);
+                               eye_region_top,eye_region_width,eye_region_height);
 		cv::Rect rightEyeRegion(face.width - eye_region_width - face.width*(kEyePercentSide/100.0),
-                          eye_region_top,eye_region_width,eye_region_height);
-
+                                eye_region_top,eye_region_width,eye_region_height);
+        
 		//-- Find Eye Centers
 		cv::Point leftPupil = findEyeCenter(faceROI,leftEyeRegion,"Left Eye");
 		cv::Point rightPupil = findEyeCenter(faceROI,rightEyeRegion,"Right Eye");
 		// get corner regions
-  cv::Rect leftRightCornerRegion(leftEyeRegion);
-  leftRightCornerRegion.width -= leftPupil.x;
-  leftRightCornerRegion.x += leftPupil.x;
-  leftRightCornerRegion.height /= 2;
-  leftRightCornerRegion.y += leftRightCornerRegion.height / 2;
-  cv::Rect leftLeftCornerRegion(leftEyeRegion);
-  leftLeftCornerRegion.width = leftPupil.x;
-  leftLeftCornerRegion.height /= 2;
-  leftLeftCornerRegion.y += leftLeftCornerRegion.height / 2;
-  cv::Rect rightLeftCornerRegion(rightEyeRegion);
-  rightLeftCornerRegion.width = rightPupil.x;
-  rightLeftCornerRegion.height /= 2;
-  rightLeftCornerRegion.y += rightLeftCornerRegion.height / 2;
-  cv::Rect rightRightCornerRegion(rightEyeRegion);
-  rightRightCornerRegion.width -= rightPupil.x;
-  rightRightCornerRegion.x += rightPupil.x;
-  rightRightCornerRegion.height /= 2;
-  rightRightCornerRegion.y += rightRightCornerRegion.height / 2;
-  rectangle(faceROI,leftRightCornerRegion,200);
-  rectangle(faceROI,leftLeftCornerRegion,200);
-  rectangle(faceROI,rightLeftCornerRegion,200);
-  rectangle(faceROI,rightRightCornerRegion,200);
-  // change eye centers to face coordinates
-  rightPupil.x += rightEyeRegion.x;
-  rightPupil.y += rightEyeRegion.y;
-  leftPupil.x += leftEyeRegion.x;
-  leftPupil.y += leftEyeRegion.y;
-  // draw eye centers
-  circle(faceROI, rightPupil, 3, 1234);
-  circle(faceROI, leftPupil, 3, 1234);
-
-  //-- Find Eye Corners
-  if (kEnableEyeCorner) {
-    cv::Point2f leftRightCorner = findEyeCorner(faceROI(leftRightCornerRegion), true, false);
-    leftRightCorner.x += leftRightCornerRegion.x;
-    leftRightCorner.y += leftRightCornerRegion.y;
-    cv::Point2f leftLeftCorner = findEyeCorner(faceROI(leftLeftCornerRegion), true, true);
-    leftLeftCorner.x += leftLeftCornerRegion.x;
-    leftLeftCorner.y += leftLeftCornerRegion.y;
-    cv::Point2f rightLeftCorner = findEyeCorner(faceROI(rightLeftCornerRegion), false, true);
-    rightLeftCorner.x += rightLeftCornerRegion.x;
-    rightLeftCorner.y += rightLeftCornerRegion.y;
-    cv::Point2f rightRightCorner = findEyeCorner(faceROI(rightRightCornerRegion), false, false);
-    rightRightCorner.x += rightRightCornerRegion.x;
-    rightRightCorner.y += rightRightCornerRegion.y;
-    circle(faceROI, leftRightCorner, 3, 200);
-    circle(faceROI, leftLeftCorner, 3, 200);
-    circle(faceROI, rightLeftCorner, 3, 200);
-    circle(faceROI, rightRightCorner, 3, 200);
-  }
-
-  imshow(face_window_name, faceROI);
-}
+        cv::Rect leftRightCornerRegion(leftEyeRegion);
+        leftRightCornerRegion.width -= leftPupil.x;
+        leftRightCornerRegion.x += leftPupil.x;
+        leftRightCornerRegion.height /= 2;
+        leftRightCornerRegion.y += leftRightCornerRegion.height / 2;
+        cv::Rect leftLeftCornerRegion(leftEyeRegion);
+        leftLeftCornerRegion.width = leftPupil.x;
+        leftLeftCornerRegion.height /= 2;
+        leftLeftCornerRegion.y += leftLeftCornerRegion.height / 2;
+        cv::Rect rightLeftCornerRegion(rightEyeRegion);
+        rightLeftCornerRegion.width = rightPupil.x;
+        rightLeftCornerRegion.height /= 2;
+        rightLeftCornerRegion.y += rightLeftCornerRegion.height / 2;
+        cv::Rect rightRightCornerRegion(rightEyeRegion);
+        rightRightCornerRegion.width -= rightPupil.x;
+        rightRightCornerRegion.x += rightPupil.x;
+        rightRightCornerRegion.height /= 2;
+        rightRightCornerRegion.y += rightRightCornerRegion.height / 2;
+        rectangle(faceROI,leftRightCornerRegion,200);
+        rectangle(faceROI,leftLeftCornerRegion,200);
+        rectangle(faceROI,rightLeftCornerRegion,200);
+        rectangle(faceROI,rightRightCornerRegion,200);
+        // change eye centers to face coordinates
+        rightPupil.x += rightEyeRegion.x;
+        rightPupil.y += rightEyeRegion.y;
+        leftPupil.x += leftEyeRegion.x;
+        leftPupil.y += leftEyeRegion.y;
+        // draw eye centers
+        circle(faceROI, rightPupil, 3, 1234);
+        circle(faceROI, leftPupil, 3, 1234);
+        
+        //-- Find Eye Corners
+        if (kEnableEyeCorner) {
+            cv::Point2f leftRightCorner = findEyeCorner(faceROI(leftRightCornerRegion), true, false);
+            leftRightCorner.x += leftRightCornerRegion.x;
+            leftRightCorner.y += leftRightCornerRegion.y;
+            cv::Point2f leftLeftCorner = findEyeCorner(faceROI(leftLeftCornerRegion), true, true);
+            leftLeftCorner.x += leftLeftCornerRegion.x;
+            leftLeftCorner.y += leftLeftCornerRegion.y;
+            cv::Point2f rightLeftCorner = findEyeCorner(faceROI(rightLeftCornerRegion), false, true);
+            rightLeftCorner.x += rightLeftCornerRegion.x;
+            rightLeftCorner.y += rightLeftCornerRegion.y;
+            cv::Point2f rightRightCorner = findEyeCorner(faceROI(rightRightCornerRegion), false, false);
+            rightRightCorner.x += rightRightCornerRegion.x;
+            rightRightCorner.y += rightRightCornerRegion.y;
+            circle(faceROI, leftRightCorner, 3, 200);
+            circle(faceROI, leftLeftCorner, 3, 200);
+            circle(faceROI, rightLeftCorner, 3, 200);
+            circle(faceROI, rightRightCorner, 3, 200);
+        }
+        
+        imshow(face_window_name, faceROI);
+    }
 }

7fc4c66c3f059753980431a46ea6730b4e6ba42e
Joel Pestana <pestanajoel@gmail.com>
2013-06-03 17:43:05 +0200

    Generate Output

---
+++ src/Output.cpp
@@ -0,0 +1,16 @@
+//
+//  Output.cpp
+//  sokaris
+//
+//  Created by Joel on 6/3/13.
+//  Copyright (c) 2013 AKH Partners. All rights reserved.
+//
+
+#include "Output.h"
+
+int Output(){
+
+
+
+
+}
---
+++ src/Output.h
@@ -0,0 +1,14 @@
+//
+//  Output.h
+//  sokaris
+//
+//  Created by Joel on 6/3/13.
+//  Copyright (c) 2013 AKH Partners. All rights reserved.
+//
+
+#ifndef __sokaris__Output__
+#define __sokaris__Output__
+
+#include <iostream>
+
+#endif /* defined(__sokaris__Output__) */

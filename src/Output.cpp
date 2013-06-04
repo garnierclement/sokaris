294e6fc8c12011d59f7aee6cb8f1dcd936e6a0f6
Joel Pestana <pestanajoel@gmail.com>
2013-06-04 09:36:09 +0200

    Output: assemblage avec Minwei et Fujia

--- src/Output.cpp
+++ src/Output.cpp
@@ -8,9 +8,33 @@
 
 #include "Output.h"
 
-int Output(){
+using namespace std;
 
+int Output(double cameraId, double x, double y, double z, double aX, double aY, double aZ){
 
-
+    stringstream sstm;
+//    float zRounded = truncf(z * 10) / 10;
+//    float angleRounded = truncf(angle * 10) / 10;
+//    
+    sstm << cameraId << " " << x << " " << y << " " << z << " " << aX << " "<< aY << " " << aZ << " "<<endl;
+    
+    string outputText = sstm.str();
+    
+    //Changer le nom du fichier pour un fichier local
+    string filename = "/Users/joel/Desktop/example";
+    
+    //Erase all file contents
+    ofstream(filename.c_str()).close();
+    
+    //Reopen the file to write in it
+    ofstream outputFile(filename.c_str(),ios::app);
+    if (outputFile.is_open())
+    {
+        outputFile << outputText;
+        outputFile.close();
+    }else{
+        return -1;
+    }
+    return 0;
 
 }
--- src/Output.h
+++ src/Output.h
@@ -9,6 +9,11 @@
 #ifndef __sokaris__Output__
 #define __sokaris__Output__
 
+#include <opencv2/opencv.hpp>
 #include <iostream>
-
+#include <fstream>
+#include <stdio.h>
+#include <vector>
+#include <iterator>
+#include <iomanip>
 #endif /* defined(__sokaris__Output__) */
